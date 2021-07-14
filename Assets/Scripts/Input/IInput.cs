7075d15e0af1a748d75e2e448e424d12e5b958ef
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-07-14 16:56:15 +0300

    added space button into input system

--- Assets/Scripts/Input/IInput.cs
+++ Assets/Scripts/Input/IInput.cs
@@ -1,10 +1,12 @@
 using System;
+using UnityEngine;
 
 namespace Asteroids.Input
 {
     public interface IInput
     {
         public event Action<float, float> OnAxisChange;
+        public event Action<bool, KeyCode> OnKeyPressed;
         public void Execute(float deltaTime);
     }
 }
--- Assets/Scripts/Input/InputKeyBoard.cs
+++ Assets/Scripts/Input/InputKeyBoard.cs
@@ -6,12 +6,14 @@ namespace Asteroids.Input
     public class InputKeyBoard: IInput
     {
         public event Action<float, float> OnAxisChange;
+        public event Action<bool, KeyCode> OnKeyPressed;
         public void Execute(float deltaTime)
         {
             var vertical = UnityEngine.Input.GetAxis(NamesManager.AXIS_VERTICAL);
             
             var horizontal = UnityEngine.Input.GetAxis(NamesManager.AXIS_HORIZONTAL);
             OnAxisChange?.Invoke(horizontal, vertical);
+            OnKeyPressed?.Invoke(UnityEngine.Input.GetKeyDown(KeyCode.Space), KeyCode.Space);
         }
     }
 }
